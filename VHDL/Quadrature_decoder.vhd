0adbed902049015085b57677cca9aea62a9b183d
GUI <sabbator@gmail.com>
2012-05-06 08:49:03 +0200

    First commit 1 OCT, GAMI version

---
+++ VHDL/Quadrature_decoder.vhd
@@ -0,0 +1,142 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+-- use IEEE.numeric_std.all;
+use IEEE.std_logic_arith.all;
+-- use IEEE.std_logic_signed.all;
+
+-- Realisation d'un decodeur de codeur incrémental à quadrature de phase.
+-- Il y a deux signaux A et B qui correspondent aux deux phases:
+--         |----------------------|
+-- --------|                      |-------------------
+--                    |------------------------|
+-- -------------------|                        |------
+-- <  1   > <    2   > <    3    > <     4    > <   1        <== Etats correspondants 
+--
+-- On peut ainsi incrémenter notre codeur en fonction des transitions:
+-- 1 <-> 3 et 2 <-> 4 sont physiquement impossible
+-- 1 -> 2 : +1      2 -> 1 : -1
+-- 2 -> 3 : +1      3 -> 2 : -1
+-- 3 -> 4 : +1      4 -> 3 : -1
+-- 4 -> 1 : +1      1 -> 4 : -1
+-- Les transistions qui correspondent à une evolution positive ( signal A en avance sur B) augmente de 1
+-- et inversement.
+
+entity Quadrature_decoder is
+	generic ( 
+		-- Nombre de ticks de clk entre deux sampling des signaux (Période d'échantillonnage). 
+		-- Doit être suffisemment petit pour ne pas perdre d'information. 
+		-- Et pas trop petit car on garde un historique des valeurs précedentes.
+		-- Le mieux: determiner la frequence max : rapport de reduction (50) * vitesse max en tour de roue/s (4) * nombre de tick/tour de codeur (500) * 4. 
+		-- Puis multiplier par 2 (critere de shanon) puis par 3 (vu qu'on a besoin d'au moins 3 valeurs dans shift), on obtient la frequence d'echantillonage (2.4MHZ).
+		-- Il reste à faire frequence d'horloge (50MHZ) / frequence d'echantillonage pour obtenir la periode en tick de clk (1.2MHZ).
+		sampling_interval : integer := 21 -- A peu pres la valeur pour le robot avec une vitesse max de 2 tours de roue par seconde (50/2.4 = 20.8). 
+	);
+	port (
+		readdata                 : out std_logic_vector(15 downto 0);                    --        readdata
+		clk                   	 : in  std_logic                     ;             --             clock.clk
+		reset                    : in  std_logic                    := '0' ;             --             reset
+		raz                      : in  std_logic                    := '0' ;             -- Remise A Zero du compteur
+		A                        : in  std_logic                    ;             -- quadrature_signal
+		B                        : in  std_logic                    ;             --                  
+		errorOut                 : out std_logic                    := '0' ;              -- gestion des erreurs
+		ledOut					    : out std_logic_vector(7 downto 0)
+	);
+end entity Quadrature_decoder;
+
+architecture arch of Quadrature_decoder is
+
+	signal counter  : signed(15 downto 0);--:= (others => '0'); -- std_logic_vector(15 downto 0)
+	
+	signal shiftA : std_logic_vector(2 downto 0);
+	signal shiftB : std_logic_vector(2 downto 0); 
+	
+	signal voteA : std_logic;
+	signal voteB : std_logic;
+	signal voteA_previous : std_logic; 
+	signal voteB_previous : std_logic;
+	
+	signal error : std_logic; -- pour gérer les cas impossibles = erreurs
+	
+	signal sortie : std_logic_vector(7 downto 0);
+	
+	--signal A : std_logic;
+	--signal B : std_logic;
+	--signal clk : std_logic;
+	--signal ledOut: std_logic_vector(7 downto 0);
+	
+begin
+
+	--A <= Encoder1_A;
+	--B <= Encoder1_B;
+	--clk <= CLOCK_50;
+	--LEDG <= ledOut ;
+
+	-- Ce process va permettre de sampler les signaux A et B
+	-- On enregistre les 3 dernières valeurs dans shiftX afin de pouvoir corriger d'eventuel erreur de lecture lors du sampling (parasites sur la ligne par exemple)
+	Shifter : process (clk, reset, A, B)
+		variable clk_tick_counter : integer range 0 to sampling_interval - 1;
+	begin
+		if reset='0' then
+			clk_tick_counter := 0;
+			shiftA <= (others => A);
+			shiftB <= (others => B);
+		elsif Rising_Edge(clk) then
+			if clk_tick_counter = sampling_interval - 1 then
+				clk_tick_counter := 0;
+				shiftA <= shiftA(1 downto 0) & A; -- On garde un historique des valeurs
+				shiftB <= shiftB(1 downto 0) & B;
+			else 
+				clk_tick_counter := clk_tick_counter + 1;
+			end if;
+		end if;
+	end process;
+	
+	-- On détermine la "vrai" valeur du signal en faisant la moyenne entière des 3 dernières valeurs. (On absorbe un eventuel sample foireux)
+	voteA <= '1' when shiftA = "110" or shiftA = "101" or shiftA = "011" or shiftA = "111" else '0';
+	voteB <= '1' when shiftB = "110" or shiftB = "101" or shiftB = "011" or shiftB = "111" else '0';
+	
+	Counter_proc : process (clk, reset, A, B, raz)
+		variable code : std_logic_vector(3 downto 0);
+		constant cstOne : signed(15 downto 0) := "0000000000000001";
+	begin
+		if reset='0' then
+			error <= '0';
+			counter <= (others => '0');
+			voteA_previous <= A;
+			voteB_previous <= B;
+		elsif raz = '1' then
+			error <= '1';
+			counter <= (others => '0');
+		elsif Rising_Edge(clk) then
+			code := voteA & voteA_previous & voteB & voteB_previous;
+			voteA_previous <= voteA;
+			voteB_previous <= voteB;
+			case code is
+				when "0000" => null; -- aucun changement
+				when "0001" => counter <= counter + cstOne; -- 4 -> 1
+				when "0010" => counter <= counter - cstOne; -- 1 -> 4
+				when "0011" => null; -- aucun changement
+				when "0100" => null;--counter <= counter - cstOne; -- 2 -> 1 -- DIVISION
+				when "0101" => error <= '1'; -- Les deux signaux se modifie en même temps -> impossible.	
+				when "0110" => error <= '1'; -- Les deux signaux se modifie en même temps -> impossible.
+				when "0111" => null;--counter <= counter + cstOne; -- 3 -> 4  -- DIVISION
+				when "1000" => null;--counter <= counter + cstOne; -- 1 -> 2  -- DIVISION
+				when "1001" => error <= '1'; -- Les deux signaux se modifie en même temps -> impossible.
+				when "1010" => error <= '1'; -- Les deux signaux se modifie en même temps -> impossible.
+				when "1011" => null;-- counter <= counter - cstOne; -- 4 -> 3 -- DIVISION
+				when "1100" => null; -- aucun changement 
+				when "1101" => counter <= counter - cstOne; -- 3 -> 2
+				when "1110" => counter <= counter + cstOne; -- 2 -> 3
+				when "1111" => null; -- aucun changement
+				when others => null;
+			end case;
+		end if;
+	end process;
+	
+	sortie <= CONV_STD_LOGIC_VECTOR(counter(7 downto 0), 8);  -- Pour voir octet faible
+	--sortie <= CONV_STD_LOGIC_VECTOR(counter(15 downto 7), 8);   -- Pour voir octet fort
+	readdata <= CONV_STD_LOGIC_VECTOR(counter, 16); -- On renvoit la valeur du compteur.
+	ledOut <= sortie;
+	errorOut <= error;
+	
+end architecture arch;
---
+++ VHDL/top_uart.vhd
@@ -0,0 +1,257 @@
+-------------------------------------------------------------------------------------------------
+ -- 1.0     ORiGiNe            2012          Modifications et nettoyage par ORiGiNe
+-------------------------------------------------------------------------------------------------
+ 
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.std_logic_arith.all;
+use IEEE.std_logic_unsigned.all;
+
+entity 	TOP_UART is
+	generic(
+		encoderNumber: integer := 2 -- nombre de codeurs /!\ Si cette valeur est modifié, il faut modifier les assignements au debut de l'archi. /!\
+	);
+	port(
+
+    -- Clocks
+    CLOCK_50 : in std_logic;          -- 50 MHz
+ 
+    -- Buttons and switches
+    KEY : in std_logic_vector(1 downto 0);         -- Push buttons
+    SW : in std_logic_vector(3 downto 0);          -- Switches
+ 
+    -- LED displays
+    LEDG : out std_logic_vector(7 downto 0);       -- Green LEDs
+ 
+    -- RS-232 interface
+    UART_TXD : out std_logic;                      -- UART transmitter
+	ArduinoFlowCtrl1_N : in std_logic; -- depuis arduino : "envoie l'octet suivant"
+	ArduinoFlowCtrl2_N : in std_logic; 
+	
+	-- Encoders interface (2 encoders)
+	Encoder1_A : in std_logic;
+	Encoder1_B : in std_logic;
+	Encoder2_A : in std_logic;
+	Encoder2_B : in std_logic
+	
+);
+end TOP_UART;
+ 
+architecture rtl of TOP_UART is
+ 
+	component miniUART 
+	port (
+		SysClk   : in  Std_Logic;  -- System Clock
+		Reset    : in  Std_Logic;  -- Reset input
+		TxD      : out Std_Logic;
+		DataIn   : in  Std_Logic_Vector(15 downto 0); -- 
+		GetFirstByte : in  Std_Logic; -- depuis arduino, octet 1
+		GetSecondByte : in  Std_Logic; -- depuis arduino, octet 2
+		ByteSent  : out Std_Logic; -- verifie octet bien envoyé
+		LoadOut  : out Std_Logic; -- debug de load
+		NextLoadOut   : out  Std_Logic;
+		FirstLoadOut   : out  Std_Logic); 
+	end component;
+	
+	component Quadrature_decoder
+	generic (
+		sampling_interval : integer
+	);
+	port (
+		readdata                 : out std_logic_vector(15 downto 0);       --        readdata
+		clk                      : in  std_logic;             --             clock.clk
+		reset                    : in  std_logic;             --             reset
+		raz                      : in  std_logic;             -- Remise A Zero du compteur
+		A                        : in  std_logic;             -- quadrature_signal
+		B                        : in  std_logic;             --                  
+		errorOut                 : out std_logic;
+		ledOut					 : out std_logic_vector(7 downto 0)
+	);
+	end component;
+	
+
+	type CommState is (Idle, ArduinoEvent1, PrepareFirstByte, SendFirstByte, FirstByteSent, ArduinoEvent2, PrepareSecondByte, SendSecondByte); -- description des etats du protocole de communication
+	type logicArray is array(1 to encoderNumber) of std_logic;
+	type shortArray is array(1 to encoderNumber) of std_logic_vector(15 downto 0);
+	
+
+	signal ArduinoFlowCtrl : logicArray;
+	signal Encoder_A : logicArray;
+	signal Encoder_B : logicArray;
+	signal DataToTransmit : shortArray;
+	signal RAZencoder : logicArray; -- RAZ du codeur
+
+	signal DataToTransmitBuffer : std_logic_vector(15 downto 0); -- le meme mais bufferisé
+	signal NewRequest : std_logic; -- premier signal venant de arduino + RAZ variable
+	signal Nextbyte : std_logic; -- Second signal venant arduino
+	signal ByteSent : std_logic;
+	
+	-- Gestion des erreurs
+	signal ArduinoFuckedUp : std_logic;
+	signal NextLoadOut : std_logic;
+	signal valeurLEDG4 : std_logic;
+	signal valeurLEDGfromEncoder : std_logic_vector(7 downto 0);
+	signal valeurLEDGdebug : std_logic_vector(7 downto 0);
+	signal devNull : std_logic;
+	signal devNullVector : std_logic_vector(15 downto 0);
+begin
+
+	-- /!\ 
+	-- Valable uniquement si encoderNumber == 2
+	ArduinoFlowCtrl <= (not ArduinoFlowCtrl1_N, not ArduinoFlowCtrl2_N);
+	Encoder_A <= (Encoder1_A, Encoder2_A);
+	Encoder_B <= (Encoder1_B, Encoder2_B);
+ 
+	U1 : miniUART 
+	PORT MAP ( 
+		SysClk   => CLOCK_50, 		--: in  Std_Logic;  -- System Clock
+		Reset    => KEY(0), 		--: in  Std_Logic;  -- Reset input
+		TxD      => UART_TXD, 		--: out Std_Logic; PIN 4 (GPIO_01)
+		DataIn   => "1010111110010110",	--DataToTransmitBuffer,--
+		GetFirstByte => NewRequest, -- pour permettre un load
+		GetSecondByte => NextByte,
+		ByteSent => ByteSent,
+		LoadOut  => valeurLEDGdebug(7),
+		NextLoadOut  => valeurLEDGdebug(6),--NextLoadOut,	
+		FirstLoadOut  => valeurLEDGdebug(5)-- devNull
+	);
+	
+	-- First encoder
+	Quadrature_decoder1: Quadrature_decoder
+	GENERIC MAP (
+		sampling_interval => 21
+	)
+	PORT MAP (
+		readdata => DataToTransmit(1),
+		clk => CLOCK_50,
+		reset => KEY(0),
+		raz => RAZencoder(1),
+		A => Encoder_A(1),
+		B => Encoder_B(1),
+		errorOut => valeurLEDGdebug(0),
+		ledOut => valeurLEDGfromEncoder --LEDG -- 
+	);
+	
+	-- Second encoder
+	Quadrature_decoder2: Quadrature_decoder
+	GENERIC MAP (
+		sampling_interval => 21
+	)
+	PORT MAP (
+		readdata => DataToTransmit(2),
+		clk => CLOCK_50,
+		reset => KEY(0),
+		raz => RAZencoder(2),
+		A => Encoder_A(2),
+		B => Encoder_B(2),
+		errorOut => valeurLEDGdebug(1) --devNull
+	);
+
+	DataBuffer : process(CLOCK_50, KEY(0))
+	variable CurrentState : CommState := Idle; -- encoder's state
+	variable CurrentEncoder : integer range 0 to encoderNumber; -- Number of current encoder between 1 and encoderNumber. Not defined => 0.
+	variable TimeOut : integer := 0;  
+	begin
+		if KEY(0) = '0' then
+			CurrentState := Idle;
+			CurrentEncoder := 0;
+			arduinoFuckedUp <= '0';
+		elsif Rising_Edge(CLOCK_50) then
+			-- interruption toutes les 0.1 secondes pour éviter un plantage permanent
+			if TimeOut <= 5000000 then 
+				TimeOut := TimeOut + 1;
+			else
+				TimeOut := 0;
+				CurrentState := Idle;
+			end if;
+			
+			case CurrentState is
+				when Idle =>
+					TimeOut := 0; -- on reinitialise
+					RAZencoder(CurrentEncoder) <= '0';
+					NextByte <= '0';
+					NewRequest <= '0';
+					CurrentEncoder := 0; -- FIXME : ArduinoFlowCtrl(2)
+			
+					if ArduinoFlowCtrl(1) = '0' and ArduinoFlowCtrl(2) = '1' then
+						CurrentState := ArduinoEvent1;
+						CurrentEncoder := 1;
+					end if;
+					-------------------------------
+					--if ArduinoFlowCtrl(1) = '0' and  ArduinoFlowCtrl(2) = '1' then
+					--	CurrentState := ArduinoEvent1;
+					--	CurrentEncoder := 1;
+					--elsif  ArduinoFlowCtrl(1) = '0' and  ArduinoFlowCtrl(1) = '1' then
+					--	CurrentState := ArduinoEvent1;
+					--	CurrentEncoder := 2;
+					--end if;
+					-------------------------------
+					-- case ArduinoFlowCtrl is
+						-- when ('0', '0') => null; -- IL NE SE PASSE RIEN
+						-- when ('1', '0') => 
+							-- CurrentState := ArduinoEvent1;
+							-- CurrentEncoder := 1;
+						-- when ('0', '1') =>
+							-- CurrentState := ArduinoEvent1;
+							-- CurrentEncoder := 2;
+						-- when others => arduinoFuckedUp <= '1';
+					-- end case;
+				
+				when ArduinoEvent1 =>
+					if ArduinoFlowCtrl(CurrentEncoder) = '1' then
+						CurrentState := PrepareFirstByte;
+					end if;
+				when PrepareFirstByte => 
+					DataToTransmitBuffer <= DataToTransmit(CurrentEncoder);
+					NewRequest <= '1';
+					CurrentState := SendFirstByte;
+				when SendFirstByte =>
+					NewRequest <= '0';
+					if ByteSent = '1' then
+						CurrentState := FirstByteSent;
+					end if;
+				when FirstByteSent => 
+					if ArduinoFlowCtrl(CurrentEncoder) = '0' then
+						CurrentState := ArduinoEvent2;
+					end if;
+				when ArduinoEvent2 => 
+					if ArduinoFlowCtrl(CurrentEncoder) = '1' then
+						CurrentState := PrepareSecondByte;
+					end if;				
+				when PrepareSecondByte => 
+					NextByte <= '1';
+					CurrentState := SendSecondByte;
+				-- ici bug aléatoire
+				when SendSecondByte => 
+					NextByte <= '0';
+					if ByteSent = '1' then
+						RAZencoder(CurrentEncoder) <= '1';
+						CurrentState := Idle; -- SecondByteSent
+					end if;
+				when others => NULL;
+			end case;
+		end if;
+	end process;
+	
+	LEDebugManager : process(SW) -- Switch le mode d'affichage des leds
+	begin
+		if SW(0) = '0' then
+			if SW(1) = '0' then
+				LEDG <=  DataToTransmit(1)(7 downto 0);
+			else
+				LEDG <=  DataToTransmit(1)(15 downto 8);
+			end if;
+		else
+			LEDG <= valeurLEDGdebug; --valeurLEDGfromEncoder;-- Pour voir les valeurs de debug
+		end if;
+	end process;
+	
+	-- Test du decodeur /!\
+	--LEDG <= DataToTransmit(1) (14 downto 7);
+	
+	UART_TXD <= 'Z'; -- signal nul
+	valeurLEDGdebug(2) <= arduinoFuckedUp;
+	valeurLEDGdebug(4) <= NextByte;--valeurLEDG4;
+	valeurLEDGdebug(3) <= DataToTransmit(1)(0);
+	--valeurLEDGdebug(6) <= ArduinoFlowCtrl(1);
+end;
